2dee8695db7e8f4ba06fbee42adc76d7500c5cb8
Jo Squire <mjsquire@digis.net>
2016-08-04 17:23:29 -0600

    add easter egg

--- magic_eight_ball.rb
+++ magic_eight_ball.rb
@@ -1,42 +1,57 @@
 
 #add easter egg key words that go to new set of answers
 require "pry"
-@answer = ['Yes', 'No', 'Ask me later', 'Go for it','haha..not on your life']
-@easter_egg = ['reset_answers','add_answers','print_answers']
+@answer = ['Yes', 'No', 'Ask me later', 'Go for it','haha..not on your life', 'thats a dumb question', 'unclear', 'I have my doubts', 'forget about it', 'who cares']
+@easter_egg = ['die', 'death', 'me']
 @answer_again = @answer.clone
 
 
 def question
   puts "Type a Yes/No question to get an answer or type 'Quit' to end."
   @question = gets.strip.downcase
-
-    if @question == "quit"
-      puts "To scared to know more about your future... Goodbye then!"
-      exit
+  array = @question.split(' ')
+    @easter_egg.length.times do |i|
+    if array.include?(@easter_egg[i])
+      puts "Aww, shut up no one cares!"
+      question
     else
       the_answer
     end
+  end
 end
 
 def the_answer
-  if @easter_egg.include? @question
-    puts "Would you like to add a better answer? (y/n)"
-    yes_no = gets.strip.downcase
+  case @question
+    when 'quit'
+      puts "To scared to know more about your future... Goodbye then!"
+      exit
+    when 'add answer'
+      puts "Would you like to add a new answer? (y/n)"
+      yes_no = gets.strip.downcase
 
-    if yes_no == 'y'
-    yes_answer
+      if yes_no == 'y'
+        yes_answer
 
-    else yes_no == 'n'
-      puts "Its wise you know Magic eight ball already has all the answers."
+      else yes_no == 'n'
+        puts "Its wise you know Magic eight ball already has all the answers."
+      end
+      question
+
+    when 'print answers'
+      @answer_again.each do |new_array|
+        puts new_array
+      end
+      question
+
+    when 'reset answers'
+      @answer_again = @answer
+      puts "Answers are back to original eight ball wisdom."
+    else
+      ans_output = @answer.sample
+      puts "Magic eight ball says #{ans_output}, and dont you dare question the eight ball."
       question
     end
-
-  else
-    ans_output = @answer.sample
-    puts "Magic eight ball very wisely says #{ans_output}, and dont you dare question the eight ball."
-    question
   end
-end
 
 def yes_answer
   puts "What answer do you want to add?"
@@ -59,7 +74,7 @@ def yes_answer
   question
 
   else yes_no == 'n'
-    puts "Its wise you know Magic eight ball already has all the answers."
+    puts "You are wise, Magic eight ball already has all the answers."
     question
   end
 end
